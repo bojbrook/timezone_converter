e40e1d5bd00a122e7b4ecbf11baca661ecac4f53
Bowen Brooks <39347269+bojbrook@users.noreply.github.com>
2024-10-26 14:47:38 -0600

    remove console.logs

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const time = require("./src");
 
-console.log(time.getTimeZones());
-
-const berlinTime = time.getCurrentTimeFromTimezone("Europe/Berlin", false);
-console.log(berlinTime); // Output: 2023-10-01T07:00:00-05:00
-
 module.exports = time;
